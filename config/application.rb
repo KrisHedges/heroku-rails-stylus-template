34847b9d9ec008ddec0212bedf0d9557a231b7c8
Kris Hedges <KrisHedges@gmail.com>
2012-03-24 19:09:41 -0400

    Added config.assets.precompile += ['csspage/cssview', 'styluspage/styusview'] also trying initialise_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,6 @@ module HerokuStylusSample
     # Enable the asset pipeline
     config.assets.precompile += ['csspage/cssview', 'styluspage/styusview']
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
